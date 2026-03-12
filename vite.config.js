e5de39ca03fef181839e6d97636fec6a3de83b7d
Abu Talha <96846228+TalhaRiaz5050@users.noreply.github.com>
2026-03-12 15:12:32 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -51,5 +51,11 @@ export default defineConfig({
   ],
   build: {
     assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+      },
+    },
   },
 });
