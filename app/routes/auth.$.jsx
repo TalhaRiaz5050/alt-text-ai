74bc0b5961c688ad28b760250dfa0a3a728fc940
Abu Talha <96846228+TalhaRiaz5050@users.noreply.github.com>
2026-03-12 15:19:48 +0500

    Update auth.$.jsx

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,6 +1,5 @@
-import { authenticate } from "../shopify.server";
+import { login } from "../shopify.server";
 
 export async function loader({ request }) {
-  await authenticate.admin(request);
-  return null;
+  return login(request);
 }
