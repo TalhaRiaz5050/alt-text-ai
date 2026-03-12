a76f6d6d3a6fe61824c0a0c0c492a9686cdbb457
Abu Talha <96846228+TalhaRiaz5050@users.noreply.github.com>
2026-03-12 15:08:07 +0500

    Update shopify.server.js

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -17,9 +17,7 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(db),
   distribution: AppDistribution.AppStore,
-  future: {
-    unstable_newEmbeddedAuthStrategy: true,
-  },
+  
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
